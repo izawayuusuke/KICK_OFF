737c949bfe5d12746d2fae354ad259b310539db4
izawayuusuke <yuu0502@gmail.com>
2020-05-05 05:24:54 +0000

    add posts_controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create]
 end
