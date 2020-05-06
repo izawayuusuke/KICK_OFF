4b66c6584df9f3efc744af79cdcd426469a24791
izawayuusuke <yuu0502@gmail.com>
2020-05-06 10:41:30 +0000

    complete likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,16 @@
 class LikesController < ApplicationController
   before_action :set_post
   def create
-    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    @like = Like.create(user_id: current_user.id, post_id: @post.id)
     @like.save
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    like = Like.find_by(user_id: current_user.id, post_id: @post.id)
     like.destroy
   end
 
   def set_post
-    @post = Post.find_by(params[:id])
+    @post = Post.find(params[:post_id])
   end
 end
