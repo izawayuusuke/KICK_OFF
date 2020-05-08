e2fe463559f0ea8b64c7b8bd71ed1dcc78f4a128
izawayuusuke <yuu0502@gmail.com>
2020-05-08 05:29:02 +0000

    test post

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
       let(:test_user) { user }
       it 'presence true' do
         user.name = ''
-        expect(user.valid?).to eq false;
+        expect(user.valid?).to eq false
       end
     end
   end
@@ -22,6 +22,15 @@ RSpec.describe User, type: :model do
     context 'Like' do
       it '1:N' do
         expect(User.reflect_on_association(:likes).macro).to eq :has_many
+      end
+    end
+
+    context 'Relationship' do
+      it 'active_relationships 1:N' do
+        expect(User.reflect_on_association(:active_relationships).macro).to eq :has_many
+      end
+      it 'passive_relationships 1:N' do
+        expect(User.reflect_on_association(:passive_relationships).macro).to eq :has_many
       end
     end
   end
