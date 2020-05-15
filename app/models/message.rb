2f80ca6d1c64289e863b10fe033d49da54e46793
izawayuusuke <yuu0502@gmail.com>
2020-05-15 12:10:00 +0000

    fix schema

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
   belongs_to :room
 
   after_create_commit { MessageBroadcastJob.perform_later self }
+  validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_043021) do
+ActiveRecord::Schema.define(version: 2020_05_15_043003) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
