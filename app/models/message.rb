c53a8b8f3dbc023a921af7643ca15d975272f5e8
izawayuusuke <yuu0502@gmail.com>
2020-05-16 03:38:37 +0000

    add action_cable with production

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
+  # create後にコミットする
   after_create_commit { MessageBroadcastJob.perform_later self }
   validates :content, presence: true
 end
