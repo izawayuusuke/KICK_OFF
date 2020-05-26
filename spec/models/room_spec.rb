313701f1ce8fc0433cf5bcb0dcae5d03abe620f9
izawayuusuke <yuu0502@gmail.com>
2020-05-26 06:30:13 +0000

    add spec models/share

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  describe 'association' do
+    context 'Entry' do
+      it '1:N' do
+        expect(Room.reflect_on_association(:entries).macro).to eq :has_many
+      end
+    end
+
+    context 'Message'do
+      it '1:N' do
+        expect(Room.reflect_on_association(:messages).macro).to eq :has_many
+      end
+    end
+  end
 end
--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Share, type: :model do
+  describe 'association' do
+    context 'User' do
+      it 'N:1' do
+        expect(Share.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Post' do
+      it 'N:1' do
+        expect(Share.reflect_on_association(:post).macro).to eq :belongs_to
+      end
+    end
+  end
 end
