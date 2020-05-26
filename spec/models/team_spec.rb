6041f656da0ef2d8fa5a57b692c968865b82cbb2
izawayuusuke <yuu0502@gmail.com>
2020-05-26 06:37:53 +0000

    add spec models/team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
+  describe 'validation' do
+    let(:team) { build(:team) }
+    context 'name' do
+      it 'presence true' do
+        team.name = ''
+        expect(team.valid?).to eq false
+      end
+    end
+  end
+
+  describe 'association' do
+    context 'Player' do
+      it '1:N' do
+        expect(Team.reflect_on_association(:players).macro).to eq :has_many
+      end
+    end
+
+    context 'Belong' do
+      it '1:N' do
+        expect(Team.reflect_on_association(:belongs).macro).to eq :has_many
+      end
+    end
+
+    context 'Discussion' do
+      it '1:N' do
+        expect(Team.reflect_on_association(:discussions).macro).to eq :has_many
+      end
+    end
+  end
 end
