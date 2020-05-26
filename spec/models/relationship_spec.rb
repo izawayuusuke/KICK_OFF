e202d945684a0bd9f7c4422631adcc3c5217ad39
izawayuusuke <yuu0502@gmail.com>
2020-05-26 06:21:10 +0000

    add spec models/relationship

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,1 +1,32 @@
 require 'rails_helper'
+
+RSpec.describe Relationship, type: :model do
+  describe 'validation' do
+    let(:relationship) { build(:relationship) }
+    context 'follower_id' do
+      it 'presence true' do
+        relationship.follower_id = ''
+        expect(relationship.valid?).to eq false
+      end
+    end
+
+    context 'followed_id' do
+      it 'presence true' do
+        relationship.followed_id = ''
+        expect(relationship.valid?).to eq false
+      end
+    end
+  end
+
+  describe 'association' do
+    context 'User' do
+      it 'follower N:1' do
+        expect(Relationship.reflect_on_association(:follower).macro).to eq :belongs_to
+      end
+
+      it 'followed N:1' do
+        expect(Relationship.reflect_on_association(:followed).macro).to eq :belongs_to
+      end
+    end
+  end
+end
