a07afe771dbd75c85538e05bb82155b1e5eef3ba
izawayuusuke <yuu0502@gmail.com>
2020-05-26 06:41:17 +0000

    add spec models/belong

--- spec/models/belong_spec.rb
+++ spec/models/belong_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Belong, type: :model do
+  describe 'association' do
+    context 'Player' do
+      it 'N:1' do
+        expect(Belong.reflect_on_association(:player).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Team' do
+      it 'N:1' do
+        expect(Belong.reflect_on_association(:team).macro).to eq :belongs_to
+      end
+    end
+  end
 end
