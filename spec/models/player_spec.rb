af90f86093105804aaa8aafb176f3e1d81b83721
izawayuusuke <yuu0502@gmail.com>
2020-05-26 06:04:40 +0000

    add spec models/player

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,4 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
+  describe 'validation' do
+    let(:player) { build(:player) }
+    context 'name' do
+      it 'presence true' do
+        player.name = ''
+        expect(player.valid?).to eq false
+      end
+    end
+
+    context 'height' do
+      it 'presence true' do
+        player.height = ''
+        expect(player.valid?).to eq false
+      end
+    end
+
+    context 'weight' do
+      it 'presence true' do
+        player.weight = ''
+        expect(player.valid?).to eq false
+      end
+    end
+
+    context 'age' do
+      it 'presence true' do
+        player.age = ''
+        expect(player.valid?).to eq false
+      end
+    end
+
+    context 'uniform_number' do
+      it 'presence true' do
+        player.uniform_number = ''
+        expect(player.valid?).to eq false
+      end
+    end
+  end
 end
