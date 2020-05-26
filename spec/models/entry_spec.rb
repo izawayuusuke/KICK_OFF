2c25b68abad22aee789eea67dc85b4ce6c16e65f
izawayuusuke <yuu0502@gmail.com>
2020-05-26 05:39:18 +0000

    add spec models/entry

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
+  describe 'association' do
+    context 'User' do
+      it 'N:1' do
+        expect(Entry.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Room' do
+      it 'N:1' do
+        expect(Entry.reflect_on_association(:room).macro).to eq :belongs_to
+      end
+    end
+  end
 end
