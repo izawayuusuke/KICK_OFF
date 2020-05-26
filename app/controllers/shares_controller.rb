b5f5d881cbf7af8fc0f308ced24461e6c7d70ee0
izawayuusuke <yuu0502@gmail.com>
2020-05-26 02:24:35 +0000

    fix unsigned_user page

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,7 +2,12 @@ class SharesController < ApplicationController
   before_action :set_post, only: [:create, :destroy]
 
   def create
-    @share = Share.create(user_id: current_user.id, post_id: @post.id)
+    if user_signed_in?
+      @share = Share.create(user_id: current_user.id, post_id: @post.id)
+    else
+      flash[:warning] = "アカウント登録もしくはログインしてください"
+      redirect_to posts_path
+    end
   end
 
   def destroy
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "Alice" }
-    email { "TEST@example.com" }
+    name { Faker::Lorem.charecters(number:10) }
+    email { Faker::Internet.email }
+    introduction { Faker::Lorem.characters(nubmer:20) }
     password { "password" }
     password_confirmation { "password"}
   end
