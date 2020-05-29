5d542b9940b7778b57d31d39de6f261cf41d396b
izawayuusuke <yuu0502@gmail.com>
2020-05-29 06:48:56 +0000

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name: "KICK OFF",
-            email: "soccer@eleven",
+            email: "kickoff@gmail.com",
             password: "kickoff",
             password_confirmation: "kickoff",
             admin: true)
@@ -25,7 +25,7 @@ end
 
 User.all.each do |user|
   user.posts.create!(user_id: user.id,
-                    content: Faker::Lorem::sentence
+                    content: Faker::Lorem::sentence)
 end
 
 users = User.all
