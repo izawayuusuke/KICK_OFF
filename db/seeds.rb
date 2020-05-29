b6fb90b07ddf772e2d2da398cd3613177556fce3
izawayuusuke <yuu0502@gmail.com>
2020-05-29 06:25:48 +0000

    fix seed user pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name: "KICK OFF",
-            email: "kick@off",
+            email: "kick@off11",
             password: "kickoff",
             password_confirmation: "kickoff",
             admin: true)
