43f584d8d29e439fee027618867520cb358a4e43
izawayuusuke <yuu0502@gmail.com>
2020-05-29 18:54:29 +0000

    add back to top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -119,3 +119,33 @@ $(document).on("turbolinks:load", function () {
   var targetOffset = $("#main").offset().top;
   $("html, body").animate({ scrollTop: targetOffset });
 });
+
+document.addEventListener("turbolinks:load", function () {
+  $(function () {
+    // #back-to-topを消す
+    $("#back-to-top").hide();
+
+    // スクロールが十分された時に#back-to-topを表示。スクロールされたら非表示
+    $(window).scroll(function () {
+      // this(window要素)がどれだけスクロールしたかをscrollTop()を使って値を取る
+      $("#pos").text($(this).scrollTop());
+      if ($(this).scrollTop() > 60) {
+        $("#back-to-top").fadeIn();
+      } else {
+        $("#back-to-top").fadeOut();
+      }
+    });
+
+    //#back-to-topがクリックされたら上に戻る
+    // animateメソッドを使用
+    $("#back-to-top a").click(function () {
+      $("html, body").animate(
+        {
+          scrollTop: 0,
+        },
+        800
+      );
+      return false;
+    });
+  });
+});
